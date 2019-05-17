558c7f94e9105e932837efd7a201e2ffb3b7e571
Galen O'Sullivan <galenosullivan@gmail.com>
2019-05-17 19:22:48 -0400

    Fix benches.
    * Remove unneeded `benchmarks` modules
    * Fix a `wrapping_add` that wasn't assigning to anything.

--- benches/bench_sorted_list.rs
+++ benches/bench_sorted_list.rs
@@ -3,61 +3,59 @@
 extern crate rand;
 extern crate sorted_collections;
 
-mod benchmarks {
-    extern crate test;
+extern crate test;
 
-    use self::test::Bencher;
-    use rand::Rng;
-    use sorted_collections::SortedList;
+use self::test::Bencher;
+use rand::Rng;
+use sorted_collections::SortedList;
 
-    #[bench]
-    fn empty(b: &mut Bencher) {
-        b.iter(|| 1)
-    }
+#[bench]
+fn empty(b: &mut Bencher) {
+    b.iter(|| 1)
+}
 
-    #[bench]
-    fn add_random_u8(b: &mut Bencher) {
-        let mut list = SortedList::default();
-        let mut rng = ::rand::thread_rng();
-        b.iter(|| list.add(rng.gen::<u8>()));
-    }
+#[bench]
+fn add_random_u8(b: &mut Bencher) {
+    let mut list = SortedList::default();
+    let mut rng = ::rand::thread_rng();
+    b.iter(|| list.add(rng.gen::<u8>()));
+}
 
-    #[bench]
-    fn add_random_u64(b: &mut Bencher) {
-        let mut list = SortedList::default();
-        let mut rng = ::rand::thread_rng();
-        b.iter(|| list.add(rng.gen::<u64>()));
-    }
+#[bench]
+fn add_random_u64(b: &mut Bencher) {
+    let mut list = SortedList::default();
+    let mut rng = ::rand::thread_rng();
+    b.iter(|| list.add(rng.gen::<u64>()));
+}
 
-    #[bench]
-    fn add_zero_u8(b: &mut Bencher) {
-        let mut list: SortedList<u8> = SortedList::default();
-        b.iter(|| list.add(0));
-    }
+#[bench]
+fn add_zero_u8(b: &mut Bencher) {
+    let mut list: SortedList<u8> = SortedList::default();
+    b.iter(|| list.add(0));
+}
 
-    #[bench]
-    fn add_zero_u64(b: &mut Bencher) {
-        let mut list: SortedList<u64> = SortedList::default();
-        b.iter(|| list.add(0));
-    }
+#[bench]
+fn add_zero_u64(b: &mut Bencher) {
+    let mut list: SortedList<u64> = SortedList::default();
+    b.iter(|| list.add(0));
+}
 
-    #[bench]
-    fn add_sequential_u8(b: &mut Bencher) {
-        let mut list = SortedList::default();
-        let mut i: u8 = 0;
-        b.iter(|| {
-            list.add(i);
-            i.wrapping_add(1)
-        });
-    }
+#[bench]
+fn add_sequential_u8(b: &mut Bencher) {
+    let mut list = SortedList::default();
+    let mut i: u8 = 0;
+    b.iter(|| {
+        list.add(i);
+        i = i.wrapping_add(1)
+    });
+}
 
-    #[bench]
-    fn add_sequential_u64(b: &mut Bencher) {
-        let mut list = SortedList::default();
-        let mut i: u64 = 0;
-        b.iter(|| {
-            list.add(i);
-            i = i + 1
-        });
-    }
+#[bench]
+fn add_increasing_u64(b: &mut Bencher) {
+    let mut list = SortedList::default();
+    let mut i: u64 = 0;
+    b.iter(|| {
+        list.add(i);
+        i = i + 1
+    });
 }
--- benches/bench_unsorted_list.rs
+++ benches/bench_unsorted_list.rs
@@ -3,72 +3,72 @@
 extern crate rand;
 extern crate sorted_collections;
 
-mod benchmarks {
-    extern crate test;
+// TODO: a macro.
+// Write each bench using the macro, then give a set of types and generate with suffixes for each.
 
+extern crate test;
 
-    use self::test::Bencher;
-    use rand::Rng;
-    use sorted_collections::UnsortedList;
+use self::test::Bencher;
+use rand::Rng;
+use sorted_collections::UnsortedList;
 
-    #[bench]
-    fn empty(b: &mut Bencher) {
-        b.iter(|| 1)
-    }
+#[bench]
+fn empty(b: &mut Bencher) {
+    b.iter(|| 1)
+}
 
-    #[bench]
-    fn push_random_u8(b: &mut Bencher) {
-        let mut list = UnsortedList::default();
-        let mut rng = ::rand::thread_rng();
-        b.iter(|| list.push(rng.gen::<u8>()));
-    }
+#[bench]
+fn push_random_u8(b: &mut Bencher) {
+    let mut list = UnsortedList::default();
+    let mut rng = ::rand::thread_rng();
+    b.iter(|| list.push(rng.gen::<u8>()));
+}
 
-    #[bench]
-    fn push_random_u64(b: &mut Bencher) {
-        let mut list = UnsortedList::default();
-        let mut rng = ::rand::thread_rng();
-        b.iter(|| list.push(rng.gen::<u64>()));
-    }
+#[bench]
+fn push_random_u64(b: &mut Bencher) {
+    let mut list = UnsortedList::default();
+    let mut rng = ::rand::thread_rng();
+    b.iter(|| list.push(rng.gen::<u64>()));
+}
 
-    #[bench]
-    fn push_zero_u8(b: &mut Bencher) {
-        let mut list: UnsortedList<u8> = UnsortedList::default();
-        b.iter(|| list.push(0));
-    }
+#[bench]
+fn push_zero_u8(b: &mut Bencher) {
+    let mut list: UnsortedList<u8> = UnsortedList::default();
+    b.iter(|| list.push(0));
+}
 
-    #[bench]
-    fn push_zero_u64(b: &mut Bencher) {
-        let mut list: UnsortedList<u64> = UnsortedList::default();
-        b.iter(|| list.push(0));
-    }
+#[bench]
+fn push_zero_u64(b: &mut Bencher) {
+    let mut list: UnsortedList<u64> = UnsortedList::default();
+    b.iter(|| list.push(0));
+}
 
-    #[bench]
-    fn push_sequential_u8(b: &mut Bencher) {
-        let mut list = UnsortedList::default();
-        let mut i: u8 = 0;
-        b.iter(|| {
-            list.push(i);
-            i = i.wrapping_add(1)
-        });
-    }
+#[bench]
+fn push_sequential_u8(b: &mut Bencher) {
+    let mut list = UnsortedList::default();
+    let mut i: u8 = 0;
+    b.iter(|| {
+        list.push(i);
+        i = i.wrapping_add(1)
+    });
+}
 
-    #[bench]
-    fn push_sequential_u64(b: &mut Bencher) {
-        let mut list = UnsortedList::default();
-        let mut i: u64 = 0;
-        b.iter(|| {
-            list.push(i);
-            i = i + 1
-        });
-    }
+#[bench]
+fn push_sequential_u64(b: &mut Bencher) {
+    let mut list = UnsortedList::default();
+    let mut i: u64 = 0;
+    b.iter(|| {
+        list.push(i);
+        i = i + 1
+    });
+}
 
-    #[bench]
-    fn insert_first_i32(b: &mut Bencher) {
-        let mut list = UnsortedList::default();
-        let mut i: i32 = 0;
-        b.iter(|| {
-            list.insert(0, i);
-            i = i.wrapping_add(1);
-        })
-    }
+#[bench]
+fn insert_first_i32(b: &mut Bencher) {
+    let mut list = UnsortedList::default();
+    let mut i: i32 = 0;
+    b.iter(|| {
+        list.insert(0, i);
+        i = i.wrapping_add(1);
+    })
 }
