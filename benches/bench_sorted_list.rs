4d413d665701a1168ef24f56bebe4b10989e7bdc
Galen O'Sullivan <galenosullivan@gmail.com>
2019-05-17 19:28:19 -0400

    Add some benchmarks for `BTreeMap` and `BTreeSet`.

--- benches/bench_sorted_list.rs
+++ benches/bench_sorted_list.rs
@@ -3,6 +3,7 @@
 extern crate rand;
 extern crate sorted_collections;
 
+use std::collections::{BTreeMap, BTreeSet};
 extern crate test;
 
 use self::test::Bencher;
@@ -15,33 +16,33 @@ fn empty(b: &mut Bencher) {
 }
 
 #[bench]
-fn add_random_u8(b: &mut Bencher) {
+fn insert_random_u8(b: &mut Bencher) {
     let mut list = SortedList::default();
     let mut rng = ::rand::thread_rng();
     b.iter(|| list.add(rng.gen::<u8>()));
 }
 
 #[bench]
-fn add_random_u64(b: &mut Bencher) {
+fn insert_random_u64(b: &mut Bencher) {
     let mut list = SortedList::default();
     let mut rng = ::rand::thread_rng();
     b.iter(|| list.add(rng.gen::<u64>()));
 }
 
 #[bench]
-fn add_zero_u8(b: &mut Bencher) {
+fn insert_zero_u8(b: &mut Bencher) {
     let mut list: SortedList<u8> = SortedList::default();
     b.iter(|| list.add(0));
 }
 
 #[bench]
-fn add_zero_u64(b: &mut Bencher) {
+fn insert_zero_u64(b: &mut Bencher) {
     let mut list: SortedList<u64> = SortedList::default();
     b.iter(|| list.add(0));
 }
 
 #[bench]
-fn add_sequential_u8(b: &mut Bencher) {
+fn insert_sequential_u8(b: &mut Bencher) {
     let mut list = SortedList::default();
     let mut i: u8 = 0;
     b.iter(|| {
@@ -51,11 +52,61 @@ fn add_sequential_u8(b: &mut Bencher) {
 }
 
 #[bench]
-fn add_increasing_u64(b: &mut Bencher) {
+fn insert_increasing_u64(b: &mut Bencher) {
     let mut list = SortedList::default();
     let mut i: u64 = 0;
     b.iter(|| {
         list.add(i);
         i = i + 1
     });
+}
+
+#[bench]
+fn insert_increasing_u64_BTreeMap(b: &mut Bencher) {
+    let mut list = BTreeMap::new();
+    let mut i: u64 = 0;
+    b.iter(|| {
+        list.insert(i, 0);
+        i = i + 1
+    })
+}
+
+#[bench]
+fn insert_increasing_u64_BTreeSet(b: &mut Bencher) {
+    let mut list = BTreeSet::new();
+    let mut i: u64 = 0;
+    b.iter(|| {
+        list.insert(i);
+        i = i + 1
+    })
+}
+
+#[bench]
+fn insert_decreasing_u64(b: &mut Bencher) {
+    let mut list = SortedList::default();
+    let mut i: u64 = std::u64::MAX;
+    b.iter(|| {
+        list.add(i);
+        i = i - 1
+    });
+}
+
+#[bench]
+fn insert_decreasing_u64_BTreeMap(b: &mut Bencher) {
+    let mut list = BTreeMap::new();
+    let mut i: u64 = std::u64::MAX;
+    b.iter(|| {
+        list.insert(i, 0);
+        i = i - 1
+    })
+}
+
+#[bench]
+fn insert_decreasing_u64_BTreeSet(b: &mut Bencher) {
+    let mut list = BTreeSet::new();
+    let mut i: u64 = std::u64::MAX;
+    b.iter(|| {
+        list.insert(i);
+        i = i - 1
+    })
 }
